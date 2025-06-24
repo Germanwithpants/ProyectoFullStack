1e2fbd8b926e8ab2ba72bf9ea60794f8c1a205dd
Germanwithpants <ric.gaete@duocuc.cl>
2025-06-23 23:47:39 -0400

    se eliminan archivos adicionales

--- springboot_crud_usuario/src/main/java/com/proyecto/_d/springbootcrud/springboot_crud_usuario/controllers/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto._d.springbootcrud.springboot_crud_usuario.controllers;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.proyecto._d.springbootcrud.springboot_crud_usuario.entities.Usuario;
-
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {}
-
