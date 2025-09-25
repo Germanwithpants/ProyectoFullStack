add79ce90ce3276578a2e1a7f65002db84c9283f
PandarRlz <164398506+PandarRlz@users.noreply.github.com>
2025-09-25 17:45:54 -0300

    Delete Usuario directory

--- Usuario/springboot_crud/src/main/java/com/proyecto/_d/springbootcrud/springboot_crud_Usuarios/controllers/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto._d.springbootcrud.springboot_crud_Usuarios.controllers;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.proyecto._d.springbootcrud.springboot_crud_Usuarios.entities.Usuario;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {}
