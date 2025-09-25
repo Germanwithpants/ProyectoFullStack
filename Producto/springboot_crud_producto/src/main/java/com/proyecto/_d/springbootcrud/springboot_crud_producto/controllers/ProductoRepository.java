ba74c743c3fe018960026fd72f57794c430a0a93
PandarRlz <164398506+PandarRlz@users.noreply.github.com>
2025-09-25 17:46:09 -0300

    Delete Producto directory

--- Producto/springboot_crud_producto/src/main/java/com/proyecto/_d/springbootcrud/springboot_crud_producto/controllers/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto._d.springbootcrud.springboot_crud_producto.controllers;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.proyecto._d.springbootcrud.springboot_crud_producto.entities.Producto;
-
-public interface ProductoRepository extends CrudRepository<Producto,Long>{
-}
